c0ccc7458c664d99dc83c5abf04ac4c42a3fab7a
Joe Heck <heckj@mac.com>
2022-08-21 15:13:15 -0700

    initial swag at impl on LWWRegister

--- Sources/CRDT/LWWRegister.swift
+++ Sources/CRDT/LWWRegister.swift
@@ -10,10 +10,10 @@ import Foundation
 /// Based on LWWRegister implementation as described in "Convergent and Commutative Replicated Data Types"
 /// - SeeAlso: [A comprehensive study of Convergent and Commutative Replicated Data Types](https://hal.inria.fr/inria-00555588/document)” by Marc Shapiro, Nuno Preguiça, Carlos Baquero, and Marek Zawirski (2011).
 public struct LWWRegister<ActorID: Hashable & Comparable, T> {
-    fileprivate struct Entry: Identifiable, PartiallyOrderable {
+    public struct Atom: Identifiable, PartiallyOrderable {
         var value: T
         var timestamp: TimeInterval
-        var id: ActorID
+        public var id: ActorID
 
         init(value: T, id: ActorID, timestamp: TimeInterval = Date().timeIntervalSinceReferenceDate) {
             self.value = value
@@ -21,19 +21,19 @@ public struct LWWRegister<ActorID: Hashable & Comparable, T> {
             self.id = id
         }
 
-        static func <= (lhs: LWWRegister<ActorID, T>.Entry, rhs: LWWRegister<ActorID, T>.Entry) -> Bool {
+        public static func <= (lhs: LWWRegister<ActorID, T>.Atom, rhs: LWWRegister<ActorID, T>.Atom) -> Bool {
             // functionally equivalent to say rhs instance is ordered after lhs instance
             (lhs.timestamp, lhs.id) <= (rhs.timestamp, rhs.id)
         }
 
         // not using Comparable because that requires T to be 'comparable' as well, but we want to be able to assert
         // partial ordering here
-        func isOrdered(after other: Entry) -> Bool {
+        func isOrdered(after other: Atom) -> Bool {
             (timestamp, id) > (other.timestamp, other.id)
         }
     }
 
-    private var entry: Entry
+    private var entry: Atom
     private var selfId: ActorID
 
     public var value: T {
@@ -41,13 +41,13 @@ public struct LWWRegister<ActorID: Hashable & Comparable, T> {
             entry.value
         }
         set {
-            entry = Entry(value: newValue, id: selfId)
+            entry = Atom(value: newValue, id: selfId)
         }
     }
 
     public init(_ value: T, actorID: ActorID) {
         selfId = actorID
-        entry = Entry(value: value, id: selfId)
+        entry = Atom(value: value, id: selfId)
     }
 }
 
@@ -60,14 +60,32 @@ extension LWWRegister: Replicable {
     }
 }
 
+extension LWWRegister: DeltaCRDT {
+//    public typealias DeltaState = Self.Atom
+//    public typealias Delta = Self.Atom
+    public var state: Atom {
+        entry
+    }
+
+    public func delta(_: Atom?) -> [Atom] {
+        [entry]
+    }
+
+    public mutating func mergeDelta(_ delta: [Atom]) {
+        if let lastDelta = delta.last {
+            entry = entry <= lastDelta ? lastDelta : entry
+        }
+    }
+}
+
 extension LWWRegister: Codable where T: Codable, ActorID: Codable {}
 
-extension LWWRegister.Entry: Codable where T: Codable, ActorID: Codable {}
+extension LWWRegister.Atom: Codable where T: Codable, ActorID: Codable {}
 
 extension LWWRegister: Equatable where T: Equatable {}
 
-extension LWWRegister.Entry: Equatable where T: Equatable {}
+extension LWWRegister.Atom: Equatable where T: Equatable {}
 
 extension LWWRegister: Hashable where T: Hashable {}
 
-extension LWWRegister.Entry: Hashable where T: Hashable {}
+extension LWWRegister.Atom: Hashable where T: Hashable {}
