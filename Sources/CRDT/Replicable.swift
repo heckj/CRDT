b6e7a1df1e78d970619d9db2e2be54d03a011493
Joe Heck <heckj@mac.com>
2022-08-21 15:23:22 -0700

    cleanup work

--- Sources/CRDT/Replicable.swift
+++ Sources/CRDT/Replicable.swift
@@ -7,7 +7,7 @@
 /// Replicable is the core feature of a Conflict-free Replicated Data Type (CRDT).
 /// Any implementation of a CRDT should provide the following guarantees of the merge:
 /// - Commutative (a -merged-> b = b -merged-> a)
-/// - Associative (a -merged-> b) -merged-> c = a -merged-> (b -merged-> c)
+/// - Associative ((a -merged-> b) -merged-> c) = (a -merged-> (b -merged-> c))
 /// - Idempotent (a -merged-> a = a)
 public protocol Replicable {
     func merged(with other: Self) -> Self
@@ -16,12 +16,11 @@ public protocol Replicable {
 /// A type that can be used to determine partial order sets or sequences.
 ///
 /// Per [Partial Order at Wolfram MathWorld](https://mathworld.wolfram.com/PartialOrder.html):
-/// 1. Reflexivity: a<=a for all a in S.
-/// 2. Antisymmetry: a<=b and b<=a implies  a=b.
-/// 3. Transitivity: a<=b and b<=c implies  a<=c.
+/// 1. Reflexivity: `a<=a` for all `a` in `S`.
+/// 2. Antisymmetry: `a<=b` and `b<=a` implies `a=b`.
+/// 3. Transitivity: `a<=b` and `b<=c` implies `a<=c`.
 public protocol PartiallyOrderable {
     static func <= (lhs: Self, rhs: Self) -> Bool
-    // func isOrdered(after other: Self) -> Bool
 }
 
 /// A type of CRDT that supports delta replication.
