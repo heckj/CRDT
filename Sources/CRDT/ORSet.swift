2adb2492b56ce3fe4c4b68d2d2214ed221b203b1
Joe Heck <heckj@mac.com>
2022-08-23 13:28:48 -0700

    initial impl of delta, needs tests

--- Sources/CRDT/ORSet.swift
+++ Sources/CRDT/ORSet.swift
@@ -23,19 +23,16 @@ public struct ORSet<ActorID: Hashable & Comparable, T: Hashable> {
         }
     }
 
-    internal var selfId: ActorID
     internal var currentTimestamp: LamportTimestamp<ActorID>
     internal var metadataByValue: [T: Metadata]
 
     public init(actorId: ActorID) {
-        selfId = actorId
         metadataByValue = .init()
-        currentTimestamp = .init(actorId: selfId)
+        currentTimestamp = .init(actorId: actorId)
     }
 
     public init(actorId: ActorID, array elements: [T]) {
-        selfId = actorId
-        self = .init(actorId: selfId)
+        self = .init(actorId: actorId)
         elements.forEach { self.insert($0) }
     }
 
@@ -102,45 +99,88 @@ extension ORSet: Replicable {
     }
 }
 
-//extension ORSet: DeltaCRDT {
-//    //    associatedtype DeltaState
-//    public struct ORSetState {
-//        let values: Set<T>
-//    }
-//
-//    //    associatedtype Delta
-//    public struct ORSetDelta {
-//        let lamportClock: LamportTimestamp<ActorID>
-//        let values: Set<T>
-//    }
-//
-//    // var state: DeltaState { get }
-//    public var state: ORSetState {
-//        _storage
-//    }
-//
-//    // func delta(_ state: DeltaState?) -> Delta
-//    public func delta(_: ORSetState?) -> ORSetDelta {
-//        [_storage]
-//    }
-//
-//    // func mergeDelta(_ delta: Delta) -> Self
-//    public func mergeDelta(_ delta: ORSetDelta) -> Self {
-//        var copy = self
-//        // Merging two grow-only sets is (conveniently) the union of the two sets
-//        let reducedSet = delta.reduce(into: Set<T>(_storage.values)) { partialResult, atom in
-//            partialResult = partialResult.union(atom.values)
-//        }
-//        copy._storage.values = reducedSet
-//        // The clock isn't used for ordering or merging, so updating it isn't strictly needed.
-//        let maxClock = delta.reduce(into: 0) { partialResult, atom in
-//            partialResult = max(partialResult, atom.clockId.clock)
-//        }
-//        copy._storage.clockId.clock = maxClock
-//        copy._storage.clockId.tick()
-//        return copy
-//    }
-//}
+extension ORSet: DeltaCRDT {
+    //    associatedtype DeltaState
+    /// The minimal state for an ORSet to compute diffs for replication.
+    public struct ORSetState {
+        let maxClockValueByActor: [ActorID: UInt64]
+    }
+
+    //    associatedtype Delta
+    /// The set of changes to bring another ORSet instance up to the same state.
+    public struct ORSetDelta {
+        let updates: [T: Metadata]
+    }
+
+    // var state: DeltaState { get }
+    /// The current state of the ORSet.
+    public var state: ORSetState {
+        // The composed, compressed state to compare consists of a list of all the collaborators (represented
+        // by the actorId in the LamportTimestamps) with their highest value for clock.
+        var maxClockValueByActor: [ActorID: UInt64]
+        maxClockValueByActor = metadataByValue.reduce(into: [:]) { partialResult, valueMetaData in
+            // Do the keys already reference an actorID?
+            if partialResult.keys.contains(valueMetaData.value.lamportTimestamp.actorId) {
+                // The keys know of this actorId, so update the value is the clock value of the timestamp is larger.
+                if let latestKnownClock = partialResult[valueMetaData.value.lamportTimestamp.actorId],
+                   latestKnownClock < valueMetaData.value.lamportTimestamp.clock
+                {
+                    partialResult[valueMetaData.value.lamportTimestamp.actorId] = valueMetaData.value.lamportTimestamp.clock
+                }
+            } else {
+                // The keys don't know of this actorId, so add this one as the latest value.
+                partialResult[valueMetaData.value.lamportTimestamp.actorId] = valueMetaData.value.lamportTimestamp.clock
+            }
+        }
+        return ORSetState(maxClockValueByActor: maxClockValueByActor)
+    }
+
+    // func delta(_ state: DeltaState?) -> Delta
+    /// Computes and returns a diff from the current state of the ORSet to be used to update another instance.
+    ///
+    /// If you don't provide a state from another ORSet instance, the returned delta represents the full state.
+    ///
+    /// - Parameter state: The optional state of the remote ORSet.
+    /// - Returns: The changes to be merged into the ORSet instance that provided the state to converge its state with this instance.
+    public func delta(_ otherInstanceState: ORSetState?) -> ORSetDelta {
+        // In the case of a null state being provided, the delta is all current values and their metadata:
+        guard let maxClockValueByActor: [ActorID: UInt64] = otherInstanceState?.maxClockValueByActor else {
+            return ORSetDelta(updates: metadataByValue)
+        }
+        // The state of a remote instance has been provided to us as a list of actorIds and max clock values.
+        var statesToReplicate: [T: Metadata]
+
+        // To determine the changes that need to be replicated to the instance that provided the state:
+        // Iterate through the local collection:
+        statesToReplicate = metadataByValue.reduce(into: [:]) { partialResult, keyMetaData in
+            // - If any actorIds are in our list that they don't have, include all values
+            if !maxClockValueByActor.keys.contains(keyMetaData.value.lamportTimestamp.actorId) {
+                partialResult[keyMetaData.key] = keyMetaData.value
+            } else
+            // - If any clock values are greater than the max clock for the actorIds they listed, provide them.
+            if let maxClockForThisActor = maxClockValueByActor[keyMetaData.value.lamportTimestamp.actorId], keyMetaData.value.lamportTimestamp.clock > maxClockForThisActor {
+                partialResult[keyMetaData.key] = keyMetaData.value
+            }
+        }
+        return ORSetDelta(updates: statesToReplicate)
+    }
+
+    // func mergeDelta(_ delta: Delta) -> Self
+    /// Returns a new instance of an ORSet with the delta you provide merged into the current ORSet.
+    /// - Parameter delta: The incremental, partial state to merge.
+    public func mergeDelta(_ delta: ORSetDelta) -> Self {
+        var copy = self
+        for (valueKey, metadata) in delta.updates {
+            copy.metadataByValue[valueKey] = metadata
+            // If the remote values have a more recent clock value for this actor instance,
+            // increment the clock.
+            if metadata.lamportTimestamp.actorId == copy.currentTimestamp.actorId, metadata.lamportTimestamp.clock > copy.currentTimestamp.clock {
+                copy.currentTimestamp.clock = metadata.lamportTimestamp.clock
+            }
+        }
+        return copy
+    }
+}
 
 extension ORSet: Codable where T: Codable, ActorID: Codable {}
 
@@ -167,7 +207,27 @@ extension ORSet.Metadata: Hashable where T: Hashable {}
                 partialResult += MemoryLayout<T>.size(ofValue: meta.key)
                 partialResult += meta.value.sizeInBytes()
             }
-            return MemoryLayout<ActorID>.size(ofValue: selfId) + currentTimestamp.sizeInBytes() + dictSize
+            return currentTimestamp.sizeInBytes() + dictSize
+        }
+    }
+
+    extension ORSet.ORSetState: ApproxSizeable {
+        public func sizeInBytes() -> Int {
+            let dictSize = maxClockValueByActor.reduce(into: 0) { partialResult, meta in
+                partialResult += MemoryLayout<ActorID>.size(ofValue: meta.key)
+                partialResult += MemoryLayout<UInt64>.size
+            }
+            return dictSize
+        }
+    }
+
+    extension ORSet.ORSetDelta: ApproxSizeable {
+        public func sizeInBytes() -> Int {
+            let dictSize = updates.reduce(into: 0) { partialResult, meta in
+                partialResult += MemoryLayout<T>.size(ofValue: meta.key)
+                partialResult += meta.value.sizeInBytes()
+            }
+            return dictSize
         }
     }
 #endif
