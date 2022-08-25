39c71536218ac4d76099237e88563b5721c4f12a
Joe Heck <heckj@mac.com>
2022-08-24 17:10:55 -0700

    initial add of example project (Xcode) structure

---
+++ Example/CRDTExample/CRDTExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CRDTExampleApp.swift
+//  CRDTExample
+//
+//  Created by Joseph Heck on 8/24/22.
+//
+
+import SwiftUI
+
+@main
+struct CRDTExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
