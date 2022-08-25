2737e51f4e7a649e3428879a9cdecb9381a4be89
Joe Heck <heckj@mac.com>
2022-08-24 17:10:55 -0700

    initial add of example project (Xcode) structure

---
+++ Example/CRDTExample/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CRDTExample
+//
+//  Created by Joseph Heck on 8/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
