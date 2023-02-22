c7aa56453c0fc683b27dc1f404cd64f026e866b9
Joe Heck <heckj@mac.com>
2023-02-21 20:19:51 -0800

    exploring how to use package-benchmark

--- ExternalBenchmarks/Benchmarks/ExternalBenchmarks/ExternalBenchmarks.swift
+++ ExternalBenchmarks/Benchmarks/ExternalBenchmarks/ExternalBenchmarks.swift
@@ -5,10 +5,38 @@ import CRDT
 
 @_dynamicReplacement(for: registerBenchmarks) // And this is how we register our benchmarks
 func benchmarks() {
-    Benchmark("List single-character append",
-              configuration: .init(metrics: BenchmarkMetric.all, throughputScalingFactor: .mega)) { benchmark in
+    Benchmark.defaultConfiguration.timeUnits = .microseconds
+    // Benchmark.defaultConfiguration.desiredIterations = .count(200)
+    Benchmark.defaultConfiguration.desiredDuration = .seconds(1)
+
+    Benchmark("Create single-character List CRDT",
+              configuration: .init(metrics: [.throughput, .wallClock], throughputScalingFactor: .kilo)) { benchmark in
         for _ in benchmark.throughputIterations {
-                    blackHole(List(actorId: "a", ["a"]))
-                }
+            blackHole(blackHole(List(actorId: "a", ["a"])))
+        }
     }
+
+    Benchmark("List six-character append",
+              configuration: .init(metrics: [.throughput, .wallClock], throughputScalingFactor: .kilo)) { benchmark in
+        for _ in benchmark.throughputIterations {
+            var mylist = List(actorId: "a", ["a"])
+            benchmark.startMeasurement()
+            mylist.append(" hello")
+            benchmark.stopMeasurement()
+        }
+    }
+
+    Benchmark("List six-character append, individual characters",
+              configuration: .init(metrics: [.throughput, .wallClock], throughputScalingFactor: .kilo)) { benchmark in
+        for _ in benchmark.throughputIterations {
+            var mylist = List(actorId: "a", ["a"])
+            let appendList = [" ", "h", "e", "l", "l", "o"]
+            benchmark.startMeasurement()
+            for val in appendList {
+                mylist.append(val)
+            }
+            benchmark.stopMeasurement()
+        }
+    }
+
 }
