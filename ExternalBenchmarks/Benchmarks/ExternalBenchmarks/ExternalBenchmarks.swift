476d6a0eeac2a0e16ffb885b09cad049dbc35027
Joe Heck <heckj@mac.com>
2023-02-26 14:50:53 -0800

    trace loading, parsing benchmarks, just to explore

--- ExternalBenchmarks/Benchmarks/ExternalBenchmarks/ExternalBenchmarks.swift
+++ ExternalBenchmarks/Benchmarks/ExternalBenchmarks/ExternalBenchmarks.swift
@@ -5,25 +5,12 @@ import CRDT
 
 @main extension BenchmarkRunner {} // Required for the main() definition to no get linker errors
 
-// https://www.donnywals.com/splitting-a-json-object-into-an-enum-and-an-associated-object-with-codable/
-
-struct EditingTrace: Decodable {
-    let operations: [RawOp]
-}
-struct RawOp: Decodable {
-    // [0, 0, "\\"],
-    let column: UInt32
-    let opType: UInt32
-    let value: String
-}
-
 enum TextOp {
-    case insert(UInt32, String)
-    case delete(UInt32, UInt32)
+    case insert(cursor: UInt32, value: String)
+    case delete(cursor: UInt32, count: UInt32)
 }
 
-
-func loadEditingTrace () async {
+func loadEditingTrace () async  -> Data {
     guard let traceURL = Bundle.module.url(forResource: "editing-trace", withExtension: "json") else {
         fatalError("Unable to find editing-trace.json in bundle")
     }
@@ -33,34 +20,87 @@ func loadEditingTrace () async {
     } catch {
         fatalError("failed load JSON data from editing-trace.json URL")
     }
-    print("[TESTING DEBUG OUTPUT] bytes loaded: \(data.count)")
-    
+    // print("[TESTING DEBUG OUTPUT] bytes loaded: \(data.count)")
+    return data
+}
+
+func parseDataIntoJSON(data: Data) async -> JSONValue {
     do {
-        let topValue = try ExtrasJSON.JSONParser().parse(bytes: data)
-        switch topValue {
-        case let .array(opsJSONValues):
-            print("Identified \(opsJSONValues.count)")
-        default:
-            print("WTF? Incorrect thing found.")
-        }
+        return try ExtrasJSON.JSONParser().parse(bytes: data)
     } catch {
         fatalError("failed parse JSON")
     }
 }
 
+func parseJSONIntoTrace(topOfTrace: JSONValue) async -> [TextOp] {
+    // var insert = 0
+    // var delete = 0
+    var trace:[TextOp] = []
+
+    switch topOfTrace {
+    case let .array(opsJSONValues):
+        // print("Identified \(opsJSONValues.count)")
+        for opsJSONValue in opsJSONValues {
+            if case let .array(internalOpValues) = opsJSONValue {
+                let cursorPos: UInt32
+                if case let .number(cursorString) = internalOpValues[0] {
+                    cursorPos = UInt32(cursorString)!
+                } else {
+                    fatalError("Invalid cursor position")
+                }
+                if case let .number(opString) = internalOpValues[1] {
+                    if opString == "0" {
+                        // insert += 1
+                        if case let .string(foundInsertString) = internalOpValues[2] {
+                            trace.append(TextOp.insert(cursor: cursorPos, value: foundInsertString))
+                        }
+                    }
+                    if opString == "1" {
+                        // delete += 1
+                        trace.append(TextOp.delete(cursor: cursorPos, count: 1))
+                    }
+                }
+            }
+        }
+    default:
+        // print("WTF? Incorrect thing found.")
+        fatalError("Top value of JSON wasn't an array.")
+    }
+    // print("[TESTING DEBUG OUTPUT] - \(insert) inserts, \(delete) deletes")
+    // print("Created trace with \(trace.count) elements")
+    return trace
+}
+
 @_dynamicReplacement(for: registerBenchmarks) // And this is how we register our benchmarks
 func benchmarks() {
-    Benchmark.defaultConfiguration.timeUnits = .microseconds
+    // Benchmark.defaultConfiguration.timeUnits = .microseconds
     Benchmark.defaultConfiguration.desiredIterations = .count(1)
-//    Benchmark.defaultConfiguration.desiredDuration = .seconds(1)
+    Benchmark.defaultConfiguration.desiredDuration = .seconds(3)
 
-    Benchmark("Process Editing Trace",
-              configuration: .init(metrics: [.throughput, .wallClock])) { benchmark in
+    Benchmark("Loading JSON trace data",
+              configuration: .init(metrics: [.throughput, .wallClock], desiredIterations: 20)) { benchmark in
         for _ in benchmark.throughputIterations {
-            await loadEditingTrace()
-            var mylist = List<String, String>(actorId: "mk")
+            blackHole(await loadEditingTrace())
+        }
+    }
+
+    Benchmark("parse JSON with Swift Extras parser",
+              configuration: .init(metrics: [.throughput, .wallClock], desiredIterations: 50)) { benchmark in
+        for _ in benchmark.throughputIterations {
+            let data = await loadEditingTrace()
             benchmark.startMeasurement()
-            mylist.append(" hello")
+            blackHole(await parseDataIntoJSON(data: data))
+            benchmark.stopMeasurement()
+        }
+    }
+
+    Benchmark("process JSON into trace data",
+              configuration: .init(metrics: [.throughput, .wallClock], desiredIterations: 30)) { benchmark in
+        for _ in benchmark.throughputIterations {
+            let data = await loadEditingTrace()
+            let jsonValue = await parseDataIntoJSON(data: data)
+            benchmark.startMeasurement()
+            blackHole(await parseJSONIntoTrace(topOfTrace: jsonValue))
             benchmark.stopMeasurement()
         }
     }
