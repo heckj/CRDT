c867c452d87f71b827f96de603eb178efc4d2966
Joe Heck <heckj@mac.com>
2023-06-20 14:23:04 -0700

    hide unsafe compiler checks behind CI env variable

--- Package.swift
+++ Package.swift
@@ -4,16 +4,23 @@ import PackageDescription
 
 var globalSwiftSettings: [PackageDescription.SwiftSetting] = []
 #if swift(>=5.7)
-    globalSwiftSettings.append(.unsafeFlags(["-Xfrontend", "-strict-concurrency=complete"]))
-    /*
-     Summation from https://www.donnywals.com/enabling-concurrency-warnings-in-xcode-14/
-     Set `strict-concurrency` to `targeted` to enforce Sendable and actor-isolation checks in your code.
-       This explicitly verifies that `Sendable` constraints are met when you mark one of your types as `Sendable`.
-       This mode is essentially a bit of a hybrid between the behavior that's intended in Swift 6, and the default in Swift 5.7.
-       Use this mode to have a bit of checking on your code that uses Swift concurrency without too many warnings and / or errors in your current codebase.
+    if ProcessInfo.processInfo.environment["CI"] != nil {
+        globalSwiftSettings.append(.unsafeFlags(["-Xfrontend", "-strict-concurrency=complete"]))
+        /*
+         Summation from https://www.donnywals.com/enabling-concurrency-warnings-in-xcode-14/
+         Set `strict-concurrency` to `targeted` to enforce Sendable and actor-isolation checks
+         in your code. This explicitly verifies that `Sendable` constraints are met when you
+         mark one of your types as `Sendable`.
+         
+         This mode is essentially a bit of a hybrid between the behavior that's intended in
+         Swift 6, and the default in Swift 5.7. Use this mode to have a bit of checking on
+         your code that uses Swift concurrency without too many warnings and / or errors in
+         your current codebase.
 
-     Set `strict-concurrency` to `complete` to get the full suite of concurrency constraints, essentially as they will work in Swift 6.
-     */
+         Set `strict-concurrency` to `complete` to get the full suite of concurrency
+         constraints, essentially as they will work in Swift 6.
+         */
+    }
 #endif
 
 let package = Package(
@@ -54,5 +61,3 @@ let package = Package(
         ),
     ]
 )
-// use: swift build -Xswiftc -Xfrontend -Xswiftc -warn-concurrency
-// to run with strict concurrency checking
